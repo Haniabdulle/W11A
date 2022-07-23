2ea4f1991afd6e1b258efe0535d2f6d102bdc49d
hani abdulle <haniamira24@gmail.com>
2022-07-23 05:38:06 -0600

    incomplete

--- selection.js
+++ selection.js
@@ -1,7 +1,7 @@
 let selction = document.getElementById("container");
 if(Cookies.get('color')){
     selction.style.backgroundColor=Cookies.get('color');
-    selction.innerHTML = "<h1> the color you choose is  " + Cookies.get('color') + "</h1>";
+    selction.innerHTML = "" + Cookies.get('color') + "</h1>";
 
 }
 
