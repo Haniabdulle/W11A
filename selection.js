a10612b5514f46f61a45ea08801750acf5a87ff2
hani abdulle <haniamira24@gmail.com>
2022-07-23 05:45:35 -0600

    not complete

--- selection.js
+++ selection.js
@@ -1,3 +1,6 @@
+// Selection value will get value of selection cookies//
+// If cookies value is one of the colours then this function will be used // 
+
 let selction = document.getElementById("container");
 if(Cookies.get('color')){
     selction.style.backgroundColor=Cookies.get('color');
@@ -5,6 +8,7 @@ if(Cookies.get('color')){
 
 }
 
+// This else statment if user doesnt choose one of the colour options //
 else{
     selction.style.backgroundColor="black";
     selction.innerHTML = "<h1> no color is selected </h1>";
